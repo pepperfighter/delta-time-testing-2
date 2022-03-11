adb5685b61848541953b90c10a4a79ed4ac910f8
pepperfighter <32882409+pepperfighter@users.noreply.github.com>
2022-03-11 09:15:48 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,36 @@
+var rocks = 0;
+
+function rockClick(number){
+  rocks = rocks + number;
+  document.getElementById("rocks").innerHTML = rocks;
+};
+
+var miners = 0;
+function buyMiner(){
+    var minerCost = Math.floor(10 * Math.pow(1.1,miners));     //works out the cost
+    if(rocks >= minerCost){                                   //checks that the player can afford
+        miners = miners + 1;                                   //increases number of miners
+      	rocks = rocks - minerCost;                          //removes the rocks spent
+        document.getElementById('miners').innerHTML = miners;  //updates the number of miners for the user
+        document.getElementById('rocks').innerHTML = rocks;  //updates the number of rocks for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,miners));       //works out the cost of the next miner
+    document.getElementById('minerCost').innerHTML = nextCost;  //updates the miner cost for the user
+};
+
+//window.setInterval(function(){
+  //rockClick(miners);
+//}, 1000);
+
+last_update_time = new Date();
+
+
+function update(){
+	
+	var curtime = new Date();
+	delta_ms = curtime.getTime() - last_update_time.getTime();
+	last_update_time.setTime(curtime.getTime());
+	
+	rocks += rps * global_mult * (delta_ms / 1000);
+	//total_goomies += gps * global_mult * (delta_ms / 1000);
+};
