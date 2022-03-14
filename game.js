ed7013fb67b5c30d448188ba4dcf3d999fc1c512
pepperfighter <32882409+pepperfighter@users.noreply.github.com>
2022-03-14 10:10:51 -0400

    Update game.js

--- game.js
+++ game.js
@@ -19,14 +19,25 @@ function buyMiner(){
 };
 
 let last_time = null;
-window.setInterval(function(){
+let total_time = 0;
+window.setInterval(function gameLoop){
   const current_time = Date.now();
   if (last_time === null){
     last_time = current_time;
   }
   const delta_time = current_time - last_time;
+  total_time += delta_time;
   last_time = current_time; //make sure to update "last_time"
 }, 1000);
+
+const currency_display = document.getElementById("currency");
+let currency = 0;
+const currency_per_millisecond = 0.001;
+
+function updateGame(delta_time, total_time){
+  currency += currency_per_millisecond * delta_time;
+  currency_display.textContext = currency.toFixed(2);
+}
 
 //window.setInterval(function(){
   //rockClick(miners);
