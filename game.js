9d916b04a7c0ef7e3776b10b4e89384f109394c0
pepperfighter <32882409+pepperfighter@users.noreply.github.com>
2022-03-14 10:28:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,26 +1,6 @@
-var rocks = 0;
-
-function rockClick(number){
-  rocks = rocks + number;
-  document.getElementById("rocks").innerHTML = rocks;
-};
-
-var miners = 0;
-function buyMiner(){
-    var minerCost = Math.floor(10 * Math.pow(1.1,miners));     //works out the cost
-    if(rocks >= minerCost){                                   //checks that the player can afford
-        miners = miners + 1;                                   //increases number of miners
-      	rocks = rocks - minerCost;                          //removes the rocks spent
-        document.getElementById('miners').innerHTML = miners;  //updates the number of miners for the user
-        document.getElementById('rocks').innerHTML = rocks;  //updates the number of rocks for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,miners));       //works out the cost of the next miner
-    document.getElementById('minerCost').innerHTML = nextCost;  //updates the miner cost for the user
-};
-
 let last_time = null;
 let total_time = 0;
-window.setInterval(function gameLoop){
+setInterval(function gameLoop){
   const current_time = Date.now();
   if (last_time === null){
     last_time = current_time;
